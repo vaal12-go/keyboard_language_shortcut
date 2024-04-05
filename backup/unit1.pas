f2a99a3a9c40e884236eefe40e6eb22440806b64
vaal12-go <vaal12@gmx.com>
2024-04-05 11:10:03 +0300

    Working change of languages _05Apr2024 for testing.

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -41,7 +41,10 @@ implementation
 procedure TForm1.FormActivate(Sender: TObject);
 begin
   //ShowMessage('Form activated');
-  windows.RegisterHotKey(self.Handle, 1, MOD_ALT, VK_4);
+  windows.RegisterHotKey(self.Handle, 1, MOD_CONTROL, VK_4);
+  windows.RegisterHotKey(self.Handle, 2, MOD_CONTROL, VK_5);
+  windows.RegisterHotKey(self.Handle, 3, MOD_CONTROL, VK_6);
+
 end;
 
 procedure TForm1.OnMenuHotKey(var Mes: TWMHotKey);
@@ -50,13 +53,28 @@ var
    forWindowHandle : HWND;
 
 begin
+ //DebugLn(String(Mes.HotKey));
+ // DebugLn(String(Mes.Msg));
  //ShowMessage('h1');
+  forWindowHandle := windows.GetForegroundWindow();
+  if(Mes.Unused = 1) then begin
+                  hk := windows.LoadKeyboardLayoutW('00000409', 0);
+    end;
+  if(Mes.Unused = 2) then begin
+                  hk := windows.LoadKeyboardLayoutW('00000419', 0);
+    end;
+    if(Mes.Unused = 3) then begin
+                  hk := windows.LoadKeyboardLayoutW('00000422', 0);
+    end;
+
  LazLogger.DebugLogger.CloseLogFileBetweenWrites:= True;
- forWindowHandle := windows.GetForegroundWindow();
+
  DebugLn('have handle');
  //self.Hide();
  //self.Show();
-  hk := windows.LoadKeyboardLayoutW('00000409', 0);
+
+//  00000419 - RUS
+//  00000422 - UKR
  windows.ActivateKeyboardLayout(hk, 0);
 
  windows.PostMessage(forWindowHandle, windows.WM_INPUTLANGCHANGEREQUEST, 0, hk);
